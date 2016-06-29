2904dd46a9adf4c826688df3ff40bd07e7c627d7
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-06-29 16:44:59 -0700

    DPN::Workers::Base - add a default timestamp

--- lib/dpn/workers/dpn_worker.rb
+++ lib/dpn/workers/dpn_worker.rb
@@ -5,6 +5,8 @@ module DPN
     class Base
       include Sidekiq::Worker
 
+      DEFAULT_TIME = Time.new(2000, 1, 1, 0, 0, 0, "+00:00").utc
+
       def perform(msg = 'you forgot a msg!')
         REDIS.lpush('dpn-messages', msg)
       end
