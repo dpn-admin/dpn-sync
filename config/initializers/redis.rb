c8c861a55ec50f20ca106c29ffa8581b0b1f809e
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-06-29 11:07:25 -0700

    Revise configs

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,11 @@
-require 'redis'
 require 'yaml'
 
-# Redis Configuration
-unless ENV['RACK_ENV'] == 'test'
-  redis_settings = YAML.load_file('config/redis.yml')
-  REDIS_CONFIG = redis_settings[ENV['RACK_ENV']].symbolize_keys
-  REDIS = Redis.new(REDIS_CONFIG)
+if ENV['RACK_ENV'] == 'test'
+  require 'fakeredis'
+else
+  require 'redis'
 end
+
+redis_settings = YAML.load_file('config/redis.yml')
+REDIS_CONFIG = redis_settings[ENV['RACK_ENV']].symbolize_keys
+REDIS = Redis.new(REDIS_CONFIG)
