365f7f9db1a0ad44916b3ec8788803ad65cc30f1
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-07-05 07:05:59 -0700

    Extract init from workers

---
+++ lib/dpn/init.rb
@@ -0,0 +1,20 @@
+require 'config'
+require 'json'
+require 'logger'
+require_relative 'workers/sync'
+require_relative 'workers/sync_bags'
+require_relative 'workers/sync_nodes'
+require_relative 'workers/node'
+require_relative 'workers/nodes'
+require_relative 'workers/sync_worker'
+require_relative 'workers/test_worker'
+
+##
+# Initialize Settings from config files
+config_files = [
+  'config/settings.yml',
+  "config/settings/#{ENV['RACK_ENV']}.yml",
+  'config/settings.local.yml',
+  "config/settings/#{ENV['RACK_ENV']}.local.yml"
+]
+Config.load_and_set_settings(config_files)
--- lib/dpn/workers.rb
+++ lib/dpn/workers.rb
@@ -1,32 +1,15 @@
-require 'config'
-require 'json'
-require 'logger'
-require_relative 'workers/sync'
-require_relative 'workers/sync_bags'
-require_relative 'workers/sync_nodes'
-require_relative 'workers/node'
-require_relative 'workers/nodes'
-require_relative 'workers/sync_worker'
-require_relative 'workers/test_worker'
+require_relative 'init'
 
 module DPN
   ##
   # DPN Workers
   module Workers
     ##
-    # Initialize Settings from config files
-    config_files = [
-      'config/settings.yml',
-      "config/settings/#{ENV['RACK_ENV']}.yml",
-      'config/settings.local.yml',
-      "config/settings/#{ENV['RACK_ENV']}.local.yml"
-    ]
-    Config.load_and_set_settings(config_files)
-
-    ##
     # Convenience class methods (must be thread safe)
     class << self
       ##
+      # Create a new instance of DPN::Workers::Nodes using the
+      # definition in Settings.nodes and Settings.local_namespace
       # @return nodes [DPN::Workers::Nodes]
       def nodes
         DPN::Workers::Nodes.new Settings.nodes.map(&:to_hash),
