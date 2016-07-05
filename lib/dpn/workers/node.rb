048cd9a9492b5036e853eb2fae2e0e1909151bb9
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-07-05 15:30:05 -0700

    DPN::Workers::Node - add specs

--- lib/dpn/workers/node.rb
+++ lib/dpn/workers/node.rb
@@ -2,36 +2,68 @@ module DPN
   module Workers
     # A wrapper for redis node data
     class Node
+      # @!attribute [r] name
+      #   @return [String]
       attr_reader :name
+      # @!attribute [r] namespace
+      #   @return [String]
       attr_reader :namespace
+      # @!attribute [r] api_root
+      #   @return [String]
       attr_reader :api_root
+      # @!attribute [r] auth_credential
+      #   @return [String]
       attr_reader :auth_credential
+      # @!attribute [r] ssh_pubkey
+      #   @return [String|nil]
       attr_reader :ssh_pubkey
+      # @!attribute [r] created_at
+      #   @return [String]
       attr_reader :created_at
+      # @!attribute [r] updated_at
+      #   @return [String]
       attr_reader :updated_at
+      # @!attribute [r] replicate_from
+      #   @return [Array<String>]
       attr_reader :replicate_from
+      # @!attribute [r] replicate_to
+      #   @return [Array<String>]
       attr_reader :replicate_to
+      # @!attribute [r] restore_from
+      #   @return [Array<String>]
       attr_reader :restore_from
+      # @!attribute [r] restore_to
+      #   @return [Array<String>]
       attr_reader :restore_to
+      # @!attribute [r] protocols
+      #   @return [Array<String>]
       attr_reader :protocols
+      # @!attribute [r] fixity_algorithms
+      #   @return [Array<String>]
       attr_reader :fixity_algorithms
+      # @!attribute [r] storage
+      #   @return [Hash]
       attr_reader :storage
 
       # @param [Hash] opts
-      # @options opts [String] :namespace
-      # @options opts [String] :api_root
-      # @options opts [String] :auth_credential
+      # @option opts [String] :namespace
+      # @option opts [String] :api_root
+      # @option opts [String] :auth_credential
       def initialize(opts)
         @namespace = opts[:namespace]
         @api_root = opts[:api_root]
         @auth_credential = opts[:auth_credential]
+        @logger = DPN::Workers.create_logger(namespace)
       end
 
+      # Test whether a node responds to a /node API request
+      # @return [True|False]
       def alive?
         response = client.node(namespace)
         response.success?
       end
 
+      # @return [DPN::Client::Agent] client
       def client
         @client ||= begin
           client = DPN::Client.client
@@ -43,31 +75,49 @@ module DPN
         end
       end
 
+      # @return [Hash]
       def to_hash
         hash = {}
         instance_variables.each do |var|
           key = var.to_s.delete('@')
           next if key == 'client'
+          next if key == 'logger'
           hash[key] = instance_variable_get(var)
         end
         hash.symbolize_keys
       end
 
+      # Set instance variables using node data from the HTTP API
+      # @return [True|False]
       def update
-        update_attributes
+        !update_attributes.empty?
       end
 
       private
 
+        # @private
+        # @!attribute [r] logger
+        #   @return [Logger]
+        attr_reader :logger
+
+        # Retrieve node data from the HTTP API
+        # @private
         def server_node_data
           response = client.node(namespace)
           raise response.body unless response.success?
           response.body
+        rescue StandardError => e
+          logger.error e.inspect
+          {}
         end
 
+        # Set instance variables using node data from the HTTP API
+        # @private
+        # @return [Hash] attributes
         def update_attributes
           server_node_data.each_pair do |key, value|
-            # Skip attributes that are explicitly initialized
+            # Skip attributes that are explicitly initialized, because all
+            # of these are required to make a successful API call.
             next if key == :api_root
             next if key == :namespace
             next if key == :auth_credential
---
+++ spec/dpn/workers/node_spec.rb
@@ -0,0 +1,79 @@
+# -*- encoding: utf-8 -*-
+require 'spec_helper'
+
+describe DPN::Workers::Node, :vcr do
+  let(:example_node) do
+    described_class.new(
+      namespace: 'example',
+      api_root: 'http://node.example.org',
+      auth_credential: 'example_token'
+    )
+  end
+
+  let(:local_node) { DPN::Workers.nodes.local_node }
+
+  let(:subject) { local_node }
+
+  describe '#new' do
+    it 'works' do
+      expect(example_node).to be_an described_class
+    end
+  end
+
+  describe '#alive?' do
+    it 'works' do
+      expect(subject.alive?).to be true
+    end
+  end
+
+  describe '#client' do
+    it 'works' do
+      expect(subject.client).not_to be_nil
+    end
+    it 'is a DPN::Client::Agent' do
+      expect(subject.client).to be_an DPN::Client::Agent
+    end
+  end
+
+  describe '#to_hash' do
+    it 'works' do
+      expect(subject.to_hash).not_to be_nil
+    end
+    it 'is a Hash' do
+      expect(subject.to_hash).to be_an Hash
+    end
+    it 'includes :namespace' do
+      expect(subject.to_hash).to include(:namespace)
+    end
+    it 'includes :api_root' do
+      expect(subject.to_hash).to include(:api_root)
+    end
+    it 'includes :auth_credential' do
+      expect(subject.to_hash).to include(:auth_credential)
+    end
+    it 'includes additional node attributes after update' do
+      expect(subject.update).to be true
+      expect(subject.to_hash).to include(:name)
+      expect(subject.to_hash).to include(:ssh_pubkey)
+      expect(subject.to_hash).to include(:created_at)
+      expect(subject.to_hash).to include(:updated_at)
+      expect(subject.to_hash).to include(:replicate_from)
+      expect(subject.to_hash).to include(:replicate_to)
+      expect(subject.to_hash).to include(:restore_from)
+      expect(subject.to_hash).to include(:restore_to)
+      expect(subject.to_hash).to include(:protocols)
+      expect(subject.to_hash).to include(:fixity_algorithms)
+      expect(subject.to_hash).to include(:storage)
+    end
+  end
+
+  describe '#update' do
+    it 'works' do
+      expect(subject.update).not_to be_nil
+    end
+    it 'is a Boolean' do
+      expect(subject.update).to be true
+      expect(example_node.update).to be false
+    end
+  end
+end
