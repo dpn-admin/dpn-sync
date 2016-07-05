86a4b7c79433b1fc85c6313d9b77f0b3509ea353
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-07-05 11:04:21 -0700

    DPN::Workers::Sync - add specs

--- lib/dpn/workers/sync.rb
+++ lib/dpn/workers/sync.rb
@@ -4,35 +4,69 @@ module DPN
     # Fetch the latest data from a remote node
     class Sync
 
-      SYNC_NAME = 'sync'.freeze
-
+      # @!attribute [r] local_node
+      #   @return [DPN::Workers::Node] a local node
       attr_reader :local_node
+
+      # @!attribute [r] local_client
+      #   @return [DPN::Client::Agent] a local client
       attr_reader :local_client
+
+      # @!attribute [r] remote_node
+      #   @return [DPN::Workers::Node] a remote node
       attr_reader :remote_node
+
+      # @!attribute [r] remote_client
+      #   @return [DPN::Client::Agent] a remote client
       attr_reader :remote_client
 
+      # Asynchronous updates are pulled from remote_node into local_node; the
+      # remote_node must have a different namespace from local_node
+      # @param local_node [DPN::Workers::Node]
+      # @param remote_node [DPN::Workers::Node]
       def initialize(local_node, remote_node)
+        raise ArgumentError if local_node.namespace == remote_node.namespace
         @local_node = local_node
         @local_client = local_node.client
         @remote_node = remote_node
         @remote_client = remote_node.client
-        @job_data = JobData.new(SYNC_NAME)
-        @logger = DPN::Workers.create_logger(SYNC_NAME)
       end
 
+      # @abstract Subclass and override {#sync} to sync node content
+      # @return [Boolean] result
       def sync
-        # TODO: implement in subclasses
+        true
       end
 
       private
 
-        attr_reader :job_data
-        attr_reader :logger
+        # @private
+        # @return [DPN::Workers::JobData]
+        def job_data
+          @job_data ||= JobData.new(job_name)
+        end
 
+        # @private
+        # @!attribute [r] job_name
+        #   @return [String]
+        def job_name
+          'sync'
+        end
+
+        # @private
+        # @return [Logger]
+        def logger
+          @logger ||= DPN::Workers.create_logger(job_name)
+        end
+
+        # @private
+        # @return [Time] last_success
         def last_success
           job_data.last_success(remote_node.namespace)
         end
 
+        # @private
+        # @return [Boolean] updated
         def last_success_update
           job_data.last_success_update(remote_node.namespace)
         end
---
+++ spec/dpn/workers/sync_spec.rb
@@ -0,0 +1,56 @@
+# -*- encoding: utf-8 -*-
+require 'spec_helper'
+
+describe DPN::Workers::Sync do
+  subject { described_class.new(local_node, remote_node) }
+
+  describe '#new' do
+    it 'works' do
+      expect(subject).not_to be_nil
+    end
+    it 'provides accessor for local_node' do
+      expect(subject.local_node).to be_an DPN::Workers::Node
+    end
+    it 'provides accessor for remote_node' do
+      expect(subject.remote_node).to be_an DPN::Workers::Node
+    end
+    it 'provides accessor for local_client' do
+      expect(subject.local_client).to be_an DPN::Client::Agent
+    end
+    it 'provides accessor for remote_client' do
+      expect(subject.remote_client).to be_an DPN::Client::Agent
+    end
+  end
+
+  describe '#sync' do
+    it 'is an abstract method that does nothing' do
+      expect(subject.sync).to be true
+    end
+  end
+
+  context 'private' do
+    describe '#job_data' do
+      let(:job_data) { subject.send(:job_data) }
+      it 'works' do
+        expect(job_data).to be_an DPN::Workers::JobData
+      end
+      it 'is used to read job data' do
+        expect(subject).to receive(:job_data).and_return(job_data)
+        expect(job_data).to receive(:last_success).with(remote_node.namespace)
+        subject.send(:last_success)
+      end
+      it 'is used to write job data' do
+        expect(subject).to receive(:job_data).and_return(job_data)
+        expect(job_data).to receive(:last_success_update).with(remote_node.namespace)
+        subject.send(:last_success_update)
+      end
+    end
+
+    describe '#logger' do
+      let(:logger) { subject.send(:logger) }
+      it 'works' do
+        expect(logger).to be_an Logger
+      end
+    end
+  end
+end
