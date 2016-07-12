d24fa2aeb4cf741de4d77aa753fabdbd8ad929a7
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-07-11 17:20:51 -0700

    WIP on capistrano deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,9 +25,16 @@ set :deploy_to, '/opt/app/dpn/dpn-sync'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push(
+  'config/redis.yml',
+  'config/settings/development.yml',
+  'config/settings.yml',
+  'config/sidekiq_schedule.yml'
+)
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -35,13 +42,29 @@ set :deploy_to, '/opt/app/dpn/dpn-sync'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-end
+##
+# Sidekiq defaults:
+#
+# :sidekiq_default_hooks => true
+# :sidekiq_pid => File.join(shared_path, 'tmp', 'pids', 'sidekiq.pid')
+# :sidekiq_env => fetch(:rack_env, fetch(:rails_env, fetch(:stage)))
+# :sidekiq_log => File.join(shared_path, 'log', 'sidekiq.log')
+# :sidekiq_options => nil
+set :sidekiq_require, File.join(current_path, 'config', 'initializers', 'sidekiq.rb')
+# :sidekiq_tag => nil
+set :sidekiq_config, 'config/sidekiq.yml'
+# :sidekiq_queue => nil
+# :sidekiq_timeout => 10
+# :sidekiq_role => :app
+set :sidekiq_processes, 2
+set :sidekiq_options_per_process, ["--queue sync_bags", "--queue sync_nodes"]
+# :sidekiq_concurrency => nil
+# :sidekiq_monit_templates_path => 'config/deploy/templates'
+# :sidekiq_monit_conf_dir => '/etc/monit/conf.d'
+# :sidekiq_monit_use_sudo => true
+# :monit_bin => '/usr/bin/monit'
+# :sidekiq_monit_default_hooks => true
+# :sidekiq_service_name => "sidekiq_#{fetch(:application)}_#{fetch(:sidekiq_env)}"
+# :sidekiq_cmd => "#{fetch(:bundle_cmd, "bundle")} exec sidekiq" # Only for capistrano2.5
+# :sidekiqctl_cmd => "#{fetch(:bundle_cmd, "bundle")} exec sidekiqctl" # Only for capistrano2.5
+# :sidekiq_user => nil #user to run sidekiq as
