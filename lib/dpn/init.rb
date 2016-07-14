f148a43cb3a85dfd8e43add79fe6312529180312
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-07-13 18:15:29 -0700

    lib/dpn/init.rb - include Bunder.require

--- lib/dpn/init.rb
+++ lib/dpn/init.rb
@@ -1,4 +1,18 @@
+require 'bundler'
+Bundler.setup
+Bundler.require
+
+##
+# Initialize Settings from config files
 require 'config'
+config_files = [
+  'config/settings.yml',
+  "config/settings/#{ENV['RACK_ENV']}.yml",
+  'config/settings.local.yml',
+  "config/settings/#{ENV['RACK_ENV']}.local.yml"
+]
+Config.load_and_set_settings(config_files)
+
 require 'json'
 require 'logger'
 require_relative 'workers/sync'
@@ -8,13 +22,3 @@ require_relative 'workers/node'
 require_relative 'workers/nodes'
 require_relative 'workers/sync_worker'
 require_relative 'workers/test_worker'
-
-##
-# Initialize Settings from config files
-config_files = [
-  'config/settings.yml',
-  "config/settings/#{ENV['RACK_ENV']}.yml",
-  'config/settings.local.yml',
-  "config/settings/#{ENV['RACK_ENV']}.local.yml"
-]
-Config.load_and_set_settings(config_files)
