a9c96babd9c5797efabfcdbeeb75da9897beb0c6
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-07-14 10:18:59 -0700

    Sidekiq - revise configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,28 +43,23 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # set :keep_releases, 5
 
 ##
-# Sidekiq defaults:
+# https://github.com/seuros/capistrano-sidekiq#usage
+# Sidekiq defaults and explicit settings
+# See also config/sidekiq.yml
 #
 # :sidekiq_default_hooks => true
 # :sidekiq_pid => File.join(shared_path, 'tmp', 'pids', 'sidekiq.pid')
 # :sidekiq_env => fetch(:rack_env, fetch(:rails_env, fetch(:stage)))
 # :sidekiq_log => File.join(shared_path, 'log', 'sidekiq.log')
 # :sidekiq_options => nil
-set :sidekiq_require, File.join(current_path, 'config', 'initializers', 'sidekiq.rb')
+set :sidekiq_require, -> { File.join(current_path, 'config', 'initializers', 'sidekiq.rb') }
 # :sidekiq_tag => nil
-set :sidekiq_config, 'config/sidekiq.yml'
+set :sidekiq_config, -> { File.join(current_path, 'config', 'sidekiq.yml') }
 # :sidekiq_queue => nil
 # :sidekiq_timeout => 10
 # :sidekiq_role => :app
 set :sidekiq_processes, 2
-set :sidekiq_options_per_process, ["--queue sync_bags", "--queue sync_nodes"]
+# set :sidekiq_options_per_process, ["--queue sync_bags", "--queue sync_nodes"]
 # :sidekiq_concurrency => nil
-# :sidekiq_monit_templates_path => 'config/deploy/templates'
-# :sidekiq_monit_conf_dir => '/etc/monit/conf.d'
-# :sidekiq_monit_use_sudo => true
-# :monit_bin => '/usr/bin/monit'
-# :sidekiq_monit_default_hooks => true
 # :sidekiq_service_name => "sidekiq_#{fetch(:application)}_#{fetch(:sidekiq_env)}"
-# :sidekiq_cmd => "#{fetch(:bundle_cmd, "bundle")} exec sidekiq" # Only for capistrano2.5
-# :sidekiqctl_cmd => "#{fetch(:bundle_cmd, "bundle")} exec sidekiqctl" # Only for capistrano2.5
 # :sidekiq_user => nil #user to run sidekiq as
--- lib/dpn/workers/sync_worker.rb
+++ lib/dpn/workers/sync_worker.rb
@@ -4,6 +4,7 @@ module DPN
     # DPN worker to sync content
     class SyncWorker
       include Sidekiq::Worker
+      sidekiq_options backtrace: 10
 
       def perform(content)
         nodes = DPN::Workers.nodes
