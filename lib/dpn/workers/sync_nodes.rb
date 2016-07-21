db8fb8c9ff62b38934530591d62b6ef92c741890
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-07-21 09:49:48 -0700

    DPN::Workers::SyncNodes - extract #save_remote_node private method

--- lib/dpn/workers/sync_nodes.rb
+++ lib/dpn/workers/sync_nodes.rb
@@ -6,14 +6,21 @@ module DPN
 
       def sync
         remote_node.update
-        response = local_client.update_node(remote_node.to_hash)
-        raise response.body unless response.success?
-        logger.info "Updated #{remote_node.namespace} node"
-        last_success_update
+        save_remote_node
       rescue StandardError => err
         logger.error err.inspect
         false
       end
+
+      private
+
+        # @return [Boolean] success of saving node information
+        def save_remote_node
+          response = local_client.update_node(remote_node.to_hash)
+          raise response.body unless response.success?
+          logger.info "Updated #{remote_node.namespace} node"
+          last_success_update
+        end
     end
   end
 end
