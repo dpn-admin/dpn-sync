f11e106811266f7b439e9753d2fea96462a89f2c
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-07-21 11:22:51 -0700

    DPN::Workers::SyncContent - enhance test coverage to 100%

--- spec/dpn/workers/sync_content_spec.rb
+++ spec/dpn/workers/sync_content_spec.rb
@@ -7,8 +7,32 @@ describe DPN::Workers::SyncContent, :vcr do
   let(:local_client) { local_node.client }
   let(:logger) { Logger.new(File::NULL) }
   subject { described_class.new content, local_client, logger }
-  before do
-    allow(subject).to receive(:content_id).and_return('content_id')
+
+  context 'with content_id' do
+    before do
+      allow(subject).to receive(:content_id).and_return('content_id')
+    end
+    it_behaves_like 'sync_content'
   end
-  it_behaves_like 'sync_content'
+
+  ##
+  # PRIVATE
+
+  describe '#content_id' do
+    it 'raises NotImplementedError' do
+      expect { subject.send(:content_id) }.to raise_error(NotImplementedError)
+    end
+  end
+
+  describe '#create' do
+    it 'raises NotImplementedError' do
+      expect { subject.send(:create) }.to raise_error(NotImplementedError)
+    end
+  end
+
+  describe '#update' do
+    it 'raises NotImplementedError' do
+      expect { subject.send(:update) }.to raise_error(NotImplementedError)
+    end
+  end
 end
