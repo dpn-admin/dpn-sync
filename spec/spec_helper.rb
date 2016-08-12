f94f964ed90b128d9dd715e9523b2d0e02ce58b4
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-08-12 13:21:47 -0700

    CodeClimate - modify VCR config to allow code-climate PUSH

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -70,10 +70,12 @@ end
 require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'fixtures/vcr_cassettes'
-  c.allow_http_connections_when_no_cassette = false
   c.hook_into :webmock
   c.default_cassette_options = {
     record: :new_episodes, # :once is default
   }
   c.configure_rspec_metadata!
+  c.allow_http_connections_when_no_cassette = false
+  c.ignore_hosts 'codeclimate.com'
 end
+
