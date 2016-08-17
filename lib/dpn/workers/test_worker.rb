00bbcddac9a946566f5edea23c2073e98443d4d0
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-08-17 16:34:51 -0700

    DPN::Workers::TestWorker - enable a test failure

--- lib/dpn/workers/test_worker.rb
+++ lib/dpn/workers/test_worker.rb
@@ -10,19 +10,13 @@ module DPN
       # @param [String] msg (defaults to: 'you forgot a msg!')
       # @return [Boolean] success
       def perform(msg = 'you forgot a msg!')
-        msg += " (processed at #{Time.now.httpdate})"
-        success = REDIS.lpush(redis_key, msg) > 0
-        raise "Failed to save message: #{msg}" unless success
-        success
-      end
-
-      private
-
-        # A redis key for storing test messages
-        # @return [String] redis_key
-        def redis_key
-          SyncSettings.sidekiq.test_message_store
+        case msg
+        when /fail/i
+          raise "Failed to save msg: #{msg}"
+        else
+          DPN::Workers::TestMessages.save msg
         end
+      end
     end
   end
 end
--- spec/dpn/workers/test_worker_spec.rb
+++ spec/dpn/workers/test_worker_spec.rb
@@ -7,7 +7,14 @@ describe DPN::Workers::TestWorker do
       result = subject.perform('msg')
       expect(result).to be true
     end
+    it 'calls DPN::Workers::TestMessages.save' do
+      expect(DPN::Workers::TestMessages).to receive(:save)
+      subject.perform('msg')
+    end
     context 'failures' do
+      it 'raises exception given a "fail" message' do
+        expect { subject.perform('fail') }.to raise_error(RuntimeError)
+      end
       it 'raises exceptions on failure' do
         expect(REDIS).to receive(:lpush).and_raise('timeout')
         expect { subject.perform('msg') }.to raise_error(RuntimeError)
