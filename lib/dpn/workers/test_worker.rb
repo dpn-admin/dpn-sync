93a00eff31d80afc146b28951327e558e583dcb3
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-08-17 12:21:43 -0700

    DPN::Workers::TestWorker - raise exception on failures

--- lib/dpn/workers/test_worker.rb
+++ lib/dpn/workers/test_worker.rb
@@ -10,9 +10,19 @@ module DPN
       # @param [String] msg (defaults to: 'you forgot a msg!')
       # @return [Boolean] success
       def perform(msg = 'you forgot a msg!')
-        msg += " (processed at #{Time.now.utc.httpdate})"
-        REDIS.lpush(SyncSettings.sidekiq.test_message_store, msg) > 0
+        msg += " (processed at #{Time.now.httpdate})"
+        success = REDIS.lpush(redis_key, msg) > 0
+        raise "Failed to save message: #{msg}" unless success
+        success
       end
+
+      private
+
+        # A redis key for storing test messages
+        # @return [String] redis_key
+        def redis_key
+          SyncSettings.sidekiq.test_message_store
+        end
     end
   end
 end
