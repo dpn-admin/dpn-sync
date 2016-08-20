f09bd56df2aa39fd9528cb38563e6f2de3a1aecb
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-08-19 18:08:16 -0700

    Sidekiq - increase the time between retries

--- lib/dpn/workers/sync_worker.rb
+++ lib/dpn/workers/sync_worker.rb
@@ -4,7 +4,13 @@ module DPN
     # DPN worker to sync content
     class SyncWorker
       include Sidekiq::Worker
-      sidekiq_options backtrace: 10
+      sidekiq_options backtrace: 10, retry: 5
+
+      # The current retry count is yielded. The return value of the block must
+      # be an integer. It is used as the delay, in seconds.
+      sidekiq_retry_in do |count|
+        10 * 60 * (count + 1) # 10 minute intervals
+      end
 
       # @param [String] class_name for a class to perform a sync process
       # @return [Boolean]
