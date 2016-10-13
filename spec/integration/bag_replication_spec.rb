cd920ba4b5f07edbc36fe7f0f8f0ba48587f9cac
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-10-12 17:35:48 -0700

    BagReplication - use a local fixture tar file for travis.ci specs

--- spec/integration/bag_replication_spec.rb
+++ spec/integration/bag_replication_spec.rb
@@ -16,6 +16,14 @@ describe DPN::Workers::BagReplication, :vcr do
   it 'works' do
     # Get a replication from an admin node for this local node.
     repl = replication_transfer
+    if ENV['TRAVIS']
+      # Replace the replication file :link with a fixture file; this is
+      # required for an integration test to pass on travis.ci
+      local_tarfile = File.join(Dir.pwd, 'fixtures', 'testbags', '00000000-0000-4000-a000-000000000003.tar')
+      expect(File.exist?(local_tarfile)).to be true
+      repl[:link] = local_tarfile
+    end
+    # Replicate the bag
     subject = described_class.new repl
     expect(subject.replicate).to be true
     bagit = subject.send(:bagit)
