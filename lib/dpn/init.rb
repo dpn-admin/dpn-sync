1ad0500742517acae7130c341de0413ed515d0c8
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-11-02 12:42:23 -0700

    Update library init

--- lib/dpn/init.rb
+++ lib/dpn/init.rb
@@ -9,25 +9,44 @@ require_relative '../../config/initializers/config'
 # DPN nodes
 require_relative 'workers/node'
 require_relative 'workers/nodes'
+
 # DPN bag replication
 require_relative 'workers/bag_paths'
 require_relative 'workers/bag_rsync'
 require_relative 'workers/bag_replication'
+
 # Sidekiq worker libraries
 require_relative 'workers/job_data'
+
 require_relative 'workers/sync'
 require_relative 'workers/sync_content'
+
 require_relative 'workers/sync_bag'
 require_relative 'workers/sync_bags'
+
+require_relative 'workers/sync_digest'
+require_relative 'workers/sync_digests'
+
+require_relative 'workers/sync_fixity'
+require_relative 'workers/sync_fixities'
+
+require_relative 'workers/sync_ingest'
+require_relative 'workers/sync_ingests'
+
 require_relative 'workers/sync_member'
 require_relative 'workers/sync_members'
+
 require_relative 'workers/sync_nodes'
+
 require_relative 'workers/sync_replication'
 require_relative 'workers/sync_replications'
+
 require_relative 'workers/test_messages'
+
 # Sidekiq workers
 require_relative 'workers/bag_worker'
 require_relative 'workers/sync_worker'
 require_relative 'workers/test_worker'
-# /is_it_working monitor
+
+# /status monitor
 require_relative 'monitor'
