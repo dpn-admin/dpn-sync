c0b22217e043c21fd02b06da472a46c18c1f1104
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-12-20 15:03:17 -0800

    Spec helper - use verified mocks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -54,6 +54,14 @@ RSpec.configure do |config|
   config.include RSpecMixin
   config.include DPN::Fixtures
 
+  config.mock_with :rspec do |mocks|
+    # This option should be set when all dependencies are being loaded
+    # before a spec run, as is the case in a typical spec helper. It will
+    # cause any verifying double instantiation for a class that does not
+    # exist to raise, protecting against incorrectly spelt names.
+    mocks.verify_doubled_constant_names = true
+  end
+
   config.before(:each) do
     allow(Logger).to receive(:new).and_return(null_logger)
     allow(Sidekiq).to receive(:logger).and_return(null_logger)
